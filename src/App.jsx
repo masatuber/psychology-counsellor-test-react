765c81f7cd8b2ea2186cc20d74519da4714c461c
masattuber <uvervivid1231@icloud.com>
2025-10-21 23:45:12 +0900

    質問データ記述する

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 
-import './App.css'
+import "./App.css";
 
 function App() {
+  // 要件定義の質問を配列で管理する
+  const questions = [
+    {
+      id: "CP1",
+      text: "人の言葉をさえぎって自分の考えを述べることがありますか？",
+    },
+    {
+      id: "CP2",
+      text: "人の話を最後まで聞かずに意見を言うことがありますか？",
+    },
+    {
+      id: "CP3",
+      text: "議論の場で自分の意見を強く主張することがありますか？",
+    },
+  ];
 
-
-  return (
-    <>
-      真っ白から始める
-    </>
-  )
+  return <>真っ白から始める</>;
 }
 
-export default App
+export default App;
