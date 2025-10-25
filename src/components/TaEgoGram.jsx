3f536fc5d84cfc2a7d7341ae3bc6219f13d8eb61
masattuber <uvervivid1231@icloud.com>
2025-10-25 22:30:39 +0900

    ベースを作成ページ分けから次回始める

--- src/components/TaEgoGram.jsx
+++ src/components/TaEgoGram.jsx
@@ -3,15 +3,15 @@ import React, { useState } from 'react'
  // 要件定義の質問を配列で管理する
 const questions = [
   {
-    id: "CP1",
+    id: "2",
     text: "人の言葉をさえぎって自分の考えを述べることがありますか？",
   },
   {
-    id: "CP2",
+    id: "1",
     text: "人の話を最後まで聞かずに意見を言うことがありますか？",
   },
   {
-    id: "CP3",
+    id: "0",
     text: "議論の場で自分の意見を強く主張することがありますか？",
   },
 ];
@@ -21,55 +21,71 @@ const TaEgoGram = () => {
   const [answers, setAnswers] = useState({});
 
   // 合計の状態管理
-  const [total, setTotal] = useState();
+  const [total, setTotal] = useState(0);
 
   //ラジオボタン変更時の処理
-  const handleChange = () => {
+  const handleChange = (id, value) => {
+    // 選択された時点で合計点を処理させる
+    setAnswers((prev) => {
+
+      const updated = {...prev, [id]: value };
+
+      const toalScore = Object.values(updated).reduce(
+        (a, b) => a + Number(b),
+        0
+      );
+
+      setTotal(toalScore);
+     // console.log(updated);
+      return updated;
+    });
 
   }
 
-  // 送信すると合計点を算出する
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  }
+
   return (
     <>
       <div>
         <h2>CP評価フォーム</h2>
-        <form onSubmit={handleSubmit}>
-          {questions.map((q) => (
-            <div key={q.id}>
-              <p>{q.text}</p>
-              <label>
-                <input type="radio"
-                  value="2" name={q.id}
-                  checked={answers[q.id] === 2}
-                  onChange={(e) => handleChange(q.id, e.target.value)}
-                />
-                ○（2点）
-              </label>
-              <label>
-                <input type="radio"
-                  value="1" name={q.id}
-                  checked={answers[q.id] === 1}
-                  onChange={(e) => (q.id, e.target.value)}
-                />
-                △（1点）
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value="0"
-                  name={q.id}
-                  checked={answers[q.id] === 0}
-                  onChange={(e) => (q.id, e.target.value)}
-                />
-                ×（0点）
-              </label>
-            </div>
-          ))}
-          <button type="submit">合計を計算する</button>
-        </form>
+
+        {questions.map((q) => (
+          <div key={q.id}>
+            <p>{q.text}</p>
+            <label>
+              <input
+                type="radio"
+                value="2"
+                name={q.id}
+                checked={answers[q.id] === "2"}
+                onChange={(e) => handleChange(q.id, e.target.value)}
+              />
+              ○（2点）
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="1"
+                name={q.id}
+                checked={answers[q.id] === "1"}
+                onChange={(e) => handleChange(q.id, e.target.value)}
+              />
+              △（1点）
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="0"
+                name={q.id}
+                checked={answers[q.id] === "0"}
+                onChange={(e) => handleChange(q.id, e.target.value)}
+              />
+              ×（0点）
+            </label>
+          </div>
+        ))}
+        <button type="submit">合計を計算する</button>
+
+        {total !== null && <div>合計点: {total}</div>}
       </div>
     </>
   );
